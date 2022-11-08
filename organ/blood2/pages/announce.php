cdf9888373d5e4d4b5d14a4644f20bf759cbc2c1
sp182 <sanjeevani.pramanik2020@vitstudent.ac.in>
2022-11-08 23:06:47 +0530

    Update announce.php

--- organ/blood2/pages/announce.php
+++ organ/blood2/pages/announce.php
@@ -13,7 +13,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>BDMS</title>
+    <title>LIVE TWICE</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -41,7 +41,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">BBMS</h1>
+                    <h1 class="page-header">LIVE TWICE</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
@@ -49,7 +49,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
-                        <div class="panel-heading">
+                        <div class="panel-heading" style="background-color: rgb(255, 179, 197)">
                             MESSAGE BOX
                         </div>
                         <div class="panel-body">
@@ -121,9 +121,7 @@ if(!$result){
 
 </body>
 
-<footer>
-        <p>&copy; <?php echo date("Y"); ?>: Developed By PARTH, NAMAN, GAURAV</p>
-    </footer>
+
 	
 	<style>
 	footer{
