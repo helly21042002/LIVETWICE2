673aa2a52a5bb314b819893a91c97f46af2d3067
helly21042002 <hellymavani@gmail.com>
2022-11-09 12:28:01 +0530

    .

--- organ/blood2/userlog/userviewblood.php
+++ organ/blood2/userlog/userviewblood.php
@@ -33,7 +33,7 @@
 <div class="container-fluid">
 <div class="row">
 <div class=".col-lg-12">
-               <h1 class="page-header">Blood Collection</h1>
+               <h1 class="page-header">Organ Collection</h1>
                 </div>
   </div>  
 
